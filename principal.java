d98c56ed3a37ac861cab3b47a1e1658274543574
jeanmael <142756890+jeanmael@users.noreply.github.com>
2023-08-23 08:32:42 -0500

    Update principal.java

--- principal.java
+++ principal.java
@@ -3,30 +3,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package javaapplication1;
-
+import java.util.Scanner;
 /**
  *
  * @author Alumno
  */
 public class Principal {
-    public static void main(String[] args){
-        Perro perro1=new Perro();
-        
-        perro1.peso=100.8;
-        System.out.println(perro1.peso);
-        
-        
-        
-        Perro perro2=new Perro("Bulldog","naranja",25,6,20);
-        perro2.setRaza("Chihuahua");
-        System.out.println(perro2.getRaza());
-        perro2.increEdad(5);
-        System.out.println(perro2);
-        
-        System.out.println(perro2);
-        
-        Diagnostico D1= new Diagnostico();
-        D1.aptoVacunaA(perro2);
-        
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Perro fido = new Perro("chihuahua", "marron");
+        Perro pelusa = new Perro("pequines", "blanco");
+
+        System.out.print("Ingrese el nuevo peso para Fido: ");
+        double nuevoPesoFido = scanner.nextDouble();
+        fido.setPeso(nuevoPesoFido);
+
+        System.out.print("Ingrese la nueva edad para Fido: ");
+        int nuevaEdadFido = scanner.nextInt();
+        fido.setEdad(nuevaEdadFido);
+
+        System.out.print("Ingrese la nueva temperatura para Fido: ");
+        double nuevaTempFido = scanner.nextDouble();
+        fido.setTemp(nuevaTempFido);
+
+        System.out.print("Ingrese el nuevo peso para Pelusa: ");
+        double nuevoPesoPelusa = scanner.nextDouble();
+        pelusa.setPeso(nuevoPesoPelusa);
+
+        System.out.print("Ingrese la nueva edad para Pelusa: ");
+        int nuevaEdadPelusa = scanner.nextInt();
+        pelusa.setEdad(nuevaEdadPelusa);
+
+        System.out.print("Ingrese la nueva temperatura para Pelusa: ");
+        double nuevaTempPelusa = scanner.nextDouble();
+        pelusa.setTemp(nuevaTempPelusa);
+
+        System.out.println("DIAGNÓSTICO Fido: " + (Diagnostico.aptoVacunaA(fido) ? "Está SANO" : "Está ENFERMO"));
+        System.out.println("DIAGNÓSTICO Pelusa: " + (Diagnostico.aptoVacunaA(pelusa) ? "Está SANO" : "Está ENFERMO"));
     }
 }
