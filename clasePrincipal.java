0806cbec22f2e86872da14839cc5283e6c5a6461
jeanmael <142756890+jeanmael@users.noreply.github.com>
2023-09-03 04:13:31 -0500

    clasePrincipal.java

---
+++ clasePrincipal.java
@@ -0,0 +1,14 @@
+public class Principal {
+    public static void main(String[] args) {
+        Rectangulo rectangulo1 = new Rectangulo(5, 10);
+        Rectangulo rectangulo2 = new Rectangulo(8, 6);
+
+        System.out.println("Información del rectángulo 1:");
+        System.out.println(rectangulo1);
+
+        System.out.println("Información del rectángulo 2:");
+        System.out.println(rectangulo2);
+
+        Rectangulo rectangulo3 = new Rectangulo(3, 7);
+        System.out.println("Información del rectángulo 3:");
+        System.out.println(rectangulo3);
