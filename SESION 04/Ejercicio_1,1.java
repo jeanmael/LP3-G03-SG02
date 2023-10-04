165c35c12b992ffd8c9160473779ba16b005871d
jeanmael <142756890+jeanmael@users.noreply.github.com>
2023-10-04 08:06:01 -0500

    Create Ejercicio_1,1.java

---
+++ SESION 04/Ejercicio_1,1.java
@@ -0,0 +1,35 @@
+public class Contacto {
+    private String nombre;
+    private String telefono;
+    private String direccion;
+
+    public Contacto(String nombre, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
