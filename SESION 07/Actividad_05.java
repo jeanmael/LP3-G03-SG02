0c647de189047b980212a41487697ac219c0e49f
jeanmael <142756890+jeanmael@users.noreply.github.com>
2023-10-18 07:53:17 -0500

    Actividad_05.java

---
+++ SESION 07/Actividad_05.java
@@ -0,0 +1,19 @@
+public class AppMiExcepcion {
+    public static void main(String[] args) {
+    }
+
+    public static void validaPropia(int 5) {
+        try {
+            minimo(n);
+            System.out.println(n + " es mayor a 10");
+        } catch (MiExcepcion e) {
+            System.out.println("No me sirve ese número");
+        }
+    }
+
+    public static void minimo(int numero) throws MiExcepcion {
+        if (numero <= 10) {
+            throw new MiExcepcion("Necesito un número mayor a 10");
+        }
+    }
+}
