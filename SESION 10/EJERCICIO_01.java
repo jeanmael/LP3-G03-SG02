b8369df273c445c3e25579bc1d861be0c785787b
jeanmael <142756890+jeanmael@users.noreply.github.com>
2023-11-13 08:47:45 -0500

    Update EJERCICIO_01.java

--- SESION 10/EJERCICIO_01.java
+++ SESION 10/EJERCICIO_01.java
@@ -1,1 +1,20 @@
+public class App {
+    public static void main(String[] args) {
+        String[] stringArray = {"Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho"};
+        System.out.printf("Array de cadenas contiene:%n");
+        imprimirArray(stringArray);
+    }
+
+    // Sobrecarga 
+    public static void imprimirArray(String[] stringArray) {
+        int columnas = 4;
+        for (int i = 0; i < stringArray.length; i++) {
+            System.out.printf("%-10s", stringArray[i]);
+            if ((i + 1) % columnas == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+}
 
