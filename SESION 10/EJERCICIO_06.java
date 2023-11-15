3ac10431b488e046d54d321d29592eaa283fc9a4
jeanmael <142756890+jeanmael@users.noreply.github.com>
2023-11-15 08:09:54 -0500

    Update EJERCICIO_06.java

--- SESION 10/EJERCICIO_06.java
+++ SESION 10/EJERCICIO_06.java
@@ -1,63 +1,74 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class Main {
+public class Copy {
+
     public static void main(String[] args) {
-        String[] arrayStrings1 = {"A", "B", "C"};
-        String[] arrayStrings2 = {"B", "C", "D"};
-
-        String[] resultadoStrings = copyArray(arrayStrings1, arrayStrings2);
-        System.out.println(Arrays.toString(resultadoStrings));
+        String[] arrayStrings1 = {"uno", "dos", "tres"};
+        String[] arrayStrings2 = {"dos", "tres", "cuatro"};
 
         Producto[] arrayProductos1 = {
-            new Producto("P001", "Laptop", 3400.0),
-            new Producto("P002", "Iphone", 1200.0)
+                new Producto("P001", "Laptop", 1200.0),
+                new Producto("P002", "Teléfono", 500.0),
         };
-
         Producto[] arrayProductos2 = {
-            new Producto("P001", "Laptop", 4200.0),
-            new Producto("P003", "Tablet", 1000.0)
+                new Producto("P002", "Teléfono", 500.0),
+                new Producto("P003", "Tablet", 300.0),
         };
-
-        Producto[] resultadoProductos = copyArray(arrayProductos1, arrayProductos2);
-        System.out.println(Arrays.toString(resultadoProductos));
 
         Persona[] arrayPersonas1 = {
-            new Persona("73456895", "John Lennon", "Yanahuara"),
-            new Persona("79458612", "George Harrison", "Cayma")
+                new Persona("123", "Juan", "Calle A"),
+                new Persona("456", "María", "Calle B")
         };
-
         Persona[] arrayPersonas2 = {
-            new Persona("73316549", "Paul McCartney", "Av. Ejercito"),
-            new Persona("76458965", "Ringo", "Av. Prada")
+                new Persona("456", "María", "Calle B"),
+                new Persona("789", "Carlos", "Calle C")
         };
 
-        Persona[] resultadoPersonas = copyArray(arrayPersonas1, arrayPersonas2);
-        System.out.println(Arrays.toString(resultadoPersonas));
+        System.out.println("Strings: " + Arrays.toString(copyArray(arrayStrings1, arrayStrings2)));
+        System.out.println("Productos: " + Arrays.toString(copyArray(arrayProductos1, arrayProductos2)));
+        System.out.println("Personas: " + Arrays.toString(copyArray(arrayPersonas1, arrayPersonas2)));
     }
 
     public static <T> T[] copyArray(T[] array1, T[] array2) {
-        T[] combinedArray = Arrays.copyOf(array1, array1.length + array2.length);
-        System.arraycopy(array2, 0, combinedArray, array1.length, array2.length);
+        List<T> resultadoList = new ArrayList<>(Arrays.asList(array1));
+        resultadoList.addAll(Arrays.asList(array2));
+        return resultadoList.toArray(Arrays.copyOf(array1, 0));
+    }
+}
 
-        int newSize = removeDuplicates(combinedArray);
-        
-        return Arrays.copyOf(combinedArray, newSize);
+class Producto {
+    private String codigo;
+    private String descripcion;
+    private double precio;
+
+    public Producto(String codigo, String descripcion, double precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
     }
 
-    private static <T> int removeDuplicates(T[] array) {
-        int newSize = 0;
-        for (int i = 0; i < array.length; i++) {
-            boolean isDuplicate = false;
-            for (int j = 0; j < newSize; j++) {
-                if (array[i].equals(array[j])) {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-            if (!isDuplicate) {
-                array[newSize++] = array[i];
-            }
-        }
-        return newSize;
+    @Override
+    public String toString() {
+        return"Producto{" + "codigo='" + codigo + "', descripcion='" + descripcion + "', precio=" + precio + '}';
+
+    }
+}
+
+class Persona {
+    private String dni;
+    private String nombre;
+    private String direccion;
+
+    public Persona(String dni, String nombre, String direccion) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "dni='" + dni + "', nombre='" + nombre + "', direccion='" + direccion + "'}";
     }
 }
