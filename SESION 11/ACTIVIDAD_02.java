4d407321d35638fbcbe96e044d094a76d15109b9
Bill Alexis Bernal Mamani <142757515+billbernal@users.noreply.github.com>
2023-11-22 08:54:13 -0500

    Update and rename ACTIVIDAD_02 to ACTIVIDAD_02.java

---
+++ SESION 11/ACTIVIDAD_02.java
@@ -0,0 +1,49 @@
+public interface Pair<K, V> {
+    K getKey();
+    V getValue();
+}
+
+public class OrderedPair<K, V> implements Pair<K, V> {
+    private K key;
+    private V value;
+
+    public OrderedPair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return this.key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + key + ", " + value + ")";
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+      
+        OrderedPair<Integer, Integer> pair1 = new OrderedPair<>(1, 10);
+        OrderedPair<Integer, String> pair2 = new OrderedPair<>(2, "Two");
+        OrderedPair<String, Persona> pair3 = new OrderedPair<>("Key", new Persona("John", "Direccion", "12345"));
+
+        System.out.println(pair1);
+        System.out.println(pair2);
+        System.out.println(pair3);
+    }
+}
+
