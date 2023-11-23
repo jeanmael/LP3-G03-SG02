37f8a135666356000bfb29f586230763bcd7a29f
jeanmael <142756890+jeanmael@users.noreply.github.com>
2023-11-23 02:04:23 -0500

    Update Actividad 06.java

--- SESION 09/Actividad 06.java
+++ SESION 09/Actividad 06.java
@@ -1,1 +1,25 @@
+//class Alumno
+//class Alumno
+public class Alumno extends Persona {
+
+    private Fecha fechaMatricula;
+
+    public Alumno(String nif, String nombre, int edad, Fecha fechaMatricula) {
+        super(nif, nombre, edad);
+        this.fechaMatricula = fechaMatricula;
+    }
+
+    public Alumno() {
+
+    }
+
+    public Fecha getFechaMatricula() {
+        return fechaMatricula;
+    }
+
+    public void setFechaMatricula(Fecha fechaMatricula) {
+        this.fechaMatricula = fechaMatricula;
+    }
+}
+
 
