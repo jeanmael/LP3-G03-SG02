241b0a625ac3e37e7dd189177d50695e69c04e59
jeanmael <142756890+jeanmael@users.noreply.github.com>
2023-11-23 01:44:28 -0500

    Update Actividad 04.java

--- SESION 09/Actividad 04.java
+++ SESION 09/Actividad 04.java
@@ -1,1 +1,46 @@
+//class TestFilePrueba
+//class TestFilePrueba
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.JFrame;
 
+public class TestFilePrueba {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream file;
+        byte b[] = new byte[1024];
+
+        try {
+            file = new FileInputStream("src/archivos/TestFile.java");
+            file.read(b);
+            String s = new String(b);
+            ViewFile view = new ViewFile(s);
+            view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            view.setSize(400, 150);
+            view.setVisible(true);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
+
+//class ViewFile
+//class ViewFile
+import javax.swing.JTextArea;
+import javax.swing.JFrame;
+
+public class ViewFile extends JFrame {
+
+    private JTextArea areaTexto;
+
+    public ViewFile(String s) {
+        super("Mostrando el contenido de un archivo");
+        areaTexto = new JTextArea(s, 5, 40);
+        add(areaTexto);
+    }
+}
