17766897762f795b09c7b6f5baa2e1c7cd70e2bd
Bill Alexis Bernal Mamani <142757515+billbernal@users.noreply.github.com>
2023-12-03 16:51:04 -0500

    Update Ejercicio_01.sql

--- SESION 12/Ejercicio_01.sql
+++ SESION 12/Ejercicio_01.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-12-2023 a las 17:22:10
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: personas
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla autores
---
 
 CREATE TABLE autores (
   id int(11) NOT NULL,
@@ -33,10 +8,6 @@ CREATE TABLE autores (
   primer_apellido varchar(50) DEFAULT NULL,
   edad int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla autores
---
 
 INSERT INTO autores (id, primer_nombre, primer_apellido, edad) VALUES
 (1, 'Jhon', 'Doe', 25),
@@ -53,27 +24,11 @@ INSERT INTO autores (id, primer_nombre, primer_apellido, edad) VALUES
 (12, 'Isabel', 'Moore', 33),
 (13, 'Jack', 'Anderson', 41);
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla autores
---
 ALTER TABLE autores
   ADD PRIMARY KEY (id);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla autores
---
 ALTER TABLE autores
   MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
